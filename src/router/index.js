37049dabe78d7c38caabd485e5a3f34268a49a0d
Sebastian Guerrero <sguerrer@andrew.cmu.edu>
2017-03-19 15:42:27 -0400

    addeed all intial routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 
 const routes = [
-  require('./routes/student.js'), require('./routes/cycle.js')
+  require('./routes/student.js'), require('./routes/cycle.js'), require('./routes/tutorial.js'), require('./routes/user.js')
 ];
 
 module.exports = function router(localApp, db){
