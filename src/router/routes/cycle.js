e33b0cee842bd4fffbfc023861b1363ea2072858
Chris Lewis <clewis3@andrew.cmu.edu>
2017-03-20 17:07:27 -0400

    individual delete for cycles

--- src/router/routes/cycle.js
+++ src/router/routes/cycle.js
@@ -4,17 +4,17 @@ module.exports = (localApp, db) => {
 	//{id: name: cycle_id: room_number: teacher_name: max_students:, students}
 	//view is tutorials/index
 	//We should refactor the count for each tutorial
-	localApp.get('/api/cycles/:id/tutotials.json', (req, res) => {
-		
+	localApp.get('/api/cycles/:id/tutorials.json', (req, res) => {
+
 	});
 
-	localApp.put('/api/cycles/:cycle_id/tutotials/:tutorial_id/.json', (req, res) => {
-		
+	localApp.put('/api/cycles/:cycle_id/tutorials/:tutorial_id/.json', (req, res) => {
+
 	});
 
 	localApp.get('/api/cycles.json', (req, res) =>{
 		db.cycle.findAll().then((cycles) => {
-			
+
 			var responseJSON = cycles.map((cycle) => {
 				return {
 					id: cycle.id,
@@ -30,6 +30,22 @@ module.exports = (localApp, db) => {
 	//note that the angula request uses id + .json
 	localApp.get('/api/cycles/:id', (req, res) => {
 
+	});
+
+
+
+	//deletes a cycle
+	localApp.delete('/api/cycles/:id', (req, res) => {
+		db.cycle.destroy({
+			where: {
+				cycle_id: req.params.id.slice(0,-5)
+			}
+		}).then((rowDeleted) => {
+			//it might be more than one, if it deletes all the associated tutorials it is in
+			if(rowDeleted >= 1) {
+				res.json({numOfRowsDeleted: rowDeleted}); //might want to change response
+			}
+		});
 	});
 
 }
